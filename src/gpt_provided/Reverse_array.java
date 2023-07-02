20bb4f43a7806fc51fbbc1e415d9f9ab02744cb2
Sofia <correabsofial@gmail.com>
2023-07-01 23:33:02 -0500

    Reverse array completed

--- src/gpt_provided/Reverse_array.java
+++ src/gpt_provided/Reverse_array.java
@@ -12,28 +12,25 @@ public class Reverse_array {
 		//Initializing the array
 		int [] numList = new int [len];
 		//Asking and assigning the numbers of the array
-		int aux = 0;//Variable to use bubble sort (?) to swap
+		int aux = 0;//Variable to use bubble sort to swap
 		for(int position = 0; position < len; position++) {
 			//Asking for the numbers contained in the array to re-arrange
 			System.out.println("enter one by one the numbers of your list");
 			numList[position] = arrayInput.nextInt();
 		}
-		for(int i = 0; i < len; i++) {	
-			for(int j = len-1; j >= i; j--) {
-				if(!(j-1<0)) {
-					System.out.println("saved num on the aux -> " + aux);
-					aux = numList[j];
-					System.out.println("num bef -> " + numList[j-1] + "\n" + "num to exchange -> " + numList[j]);
-					numList[j] = numList[j-1];
-					numList[j-1] = aux;
-				}
+		for(int i = 0; i < len; i++) {	//To control repetition
+			for(int j = len-1; j > i; j--) {//To swap elements in the array
+				aux = numList[j];//Saves the desired element to swap to empty the "box"
+				//In the emptied position adds the number before it emptying that "box"
+				numList[j] = numList[j-1];
+				numList[j-1] = aux;//The fills the position before the actual with the saved value 
 			}
-			System.out.println();
-			System.out.println("----iteration end----");
 		}
-		
+
+		//Prints re-arranged array
 		for(int i = 0; i < len; i++) {
 			System.out.println(numList[i]);
 		}
+		arrayInput.close();
 	}
 }
